ad6fc19d4f944ed15d182c7d6cb06cf5b3d0a484
Gareth <gmoore24@qub.ac.uk>
2017-07-28 10:57:46 +0100

    Third Commit

--- app/src/main/java/com/example/gareth/speakitvisualcommunication/MainScreen.java
+++ app/src/main/java/com/example/gareth/speakitvisualcommunication/MainScreen.java
@@ -9,26 +9,28 @@ import java.util.Arrays;
 import java.util.List;
 
 public class MainScreen extends AppCompatActivity {
+    List<PecsImages> imageCategories;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_screen);
 
-        List<PecsImages> imageCategories = new ArrayList<>(Arrays.asList(categories));
+        imageCategories = new ArrayList<>(Arrays.asList(categories));
         final GridView gridView = (GridView)findViewById(R.id.gridview);
         final ImageAdapter imageAdapter = new ImageAdapter(this, imageCategories);
         gridView.setAdapter(imageAdapter);
+
     }
 
     private PecsImages[] categories = {
+            new PecsImages(R.string.Add_Category, R.mipmap.ic_launcher),
+            new PecsImages(R.string.Favourites, R.mipmap.ic_launcher),
             new PecsImages(R.string.At_Home, R.drawable.home),
             new PecsImages(R.string.About_Me, R.mipmap.ic_launcher),
             new PecsImages(R.string.Food_And_Drink, R.mipmap.ic_launcher),
             new PecsImages(R.string.Greetings, R.mipmap.ic_launcher),
             new PecsImages(R.string.Leisure, R.mipmap.ic_launcher),
-            new PecsImages(R.string.Favourites, R.mipmap.ic_launcher),
-            new PecsImages(R.string.Todays_Activities, R.mipmap.ic_launcher),
-            new PecsImages(R.string.Add_Category, R.mipmap.ic_launcher)
+            new PecsImages(R.string.Todays_Activities, R.mipmap.ic_launcher)
     };
 }
